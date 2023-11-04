d507047ccfb683589ae8fcad164606931635d732
mattyhempstead <matty.hempstead@gmail.com>
2023-11-04 21:10:49 +1100

    Better JS

--- src/js/content.js
+++ src/js/content.js
@@ -1,50 +1,53 @@
 console.log('Smooth Scroll');
 
-const scrollSpeed = 16
-const scrollSpeedFast = 32
+// Pixels per second
+const scrollSpeed = 12;
 
+// Pixels per second in fast mode
+const scrollSpeedFast = 32;
 
-let fastScroll = false
+// Fast scroll active
+let fastScroll = false;
 
-let scrollActivated = false
+let scrollActivated = false;
 
-let scrollUp = false
-let scrollDown = false
+let scrollUp = false;
+let scrollDown = false;
 
 
 function scroll() {
   if (scrollDown || scrollUp) {
     window.scrollBy({
       top: (fastScroll ? scrollSpeedFast : scrollSpeed) * (scrollDown - scrollUp)
-    })
-    window.requestAnimationFrame(scroll)
+    });
+    window.requestAnimationFrame(scroll);
   } else {
-    scrollActivated = false
+    scrollActivated = false;
   }
 }
 
 
 window.addEventListener('keydown', evt => {
   if (evt.code === 'ArrowDown' && evt.shiftKey) {
-    scrollDown = true
-    if (!scrollActivated) scroll()
-    scrollActivated = true
+    scrollDown = true;
+    if (!scrollActivated) scroll();
+    scrollActivated = true;
   } else if (evt.code === 'ArrowUp' && evt.shiftKey) {
-    scrollUp = true
-    if (!scrollActivated) scroll()
-    scrollActivated = true
+    scrollUp = true;
+    if (!scrollActivated) scroll();
+    scrollActivated = true;
   } else if (evt.key === 'Control') {
-    fastScroll = true
+    fastScroll = true;
   }
 })
 
 window.addEventListener('keyup', evt => {
   if (evt.code === 'ArrowDown') {
-    scrollDown = false 
+    scrollDown = false;
   } else if (evt.code === 'ArrowUp') {
-    scrollUp = false
+    scrollUp = false;
   } else if (evt.key === 'Control') {
-    fastScroll = false
+    fastScroll = false;
   }
 })
 
